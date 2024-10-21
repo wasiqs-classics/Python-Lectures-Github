196d1c47d7debf09e65ceebcab22752643d0009c
wasiqs-classics <was_iq87@hotmail.com>
2024-10-21 14:15:17 +0500

    Added the supported code file

---
+++ Module 1 - Basic Topics/002 Variables and Data Types/datatypes.py
@@ -0,0 +1,47 @@
+# Common Types
+
+# String
+name = "Wasiq"
+
+# Integer
+age = 37
+
+# Float
+salary = 2000.00
+
+# Boolean
+is_Married = True
+
+# Checking for Type of the variable
+print(f"The type of the name variable is: {type(name)}")
+print(f"The type of the salary variable is: {type(salary)}")
+
+# Type Conversion
+
+salary = int(salary)
+
+# Now checking the type again. 
+print(f"The type of the salary variable is: {type(salary)}")
+
+# Constants
+PI = 3.14159
+GRAVITY = 9.8
+
+# Collectable Data Types
+
+# List
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4.5]
+
+# Dictionary
+student = {"name": "Alice", "age": 20, "grade": "A"}
+
+# Tuple
+coordinates = (10, 20)
+
+# Set
+unique_numbers = {1, 2, 3, 4}
+
+# Type Hints
+first_name: str = "Wasiq" # Here we have already declared the type
+last_name: str = "Khan"
