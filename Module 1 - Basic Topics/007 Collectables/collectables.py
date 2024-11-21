115562974e7414b0fa0feb5c2532b714965d1f6e
wasiqs-classics <was_iq87@hotmail.com>
2024-11-21 23:33:52 +0500

    updated collectables

--- Module 1 - Basic Topics/007 Collectables/collectables.py
+++ Module 1 - Basic Topics/007 Collectables/collectables.py
@@ -66,4 +66,52 @@ print(student_details)
 print(student_details["education"]["University"])
 print(student_details["Skills"][1])
 
-print(student_details.get("age", "Not found")) # a safer method! 
+print(student_details.get("phone")) # a safer method! 
+
+
+print(student_details.pop("age"))
+
+print(student_details)
+
+del student_details["Skills"]
+
+print(student_details)
+
+student_details["experience"] = "6 years"
+
+print(student_details)
+
+print(student_details.keys())
+print(student_details.values())
+print(student_details.items())
+
+print(len(student_details.keys()))
+
+#Tuples
+
+location = (50.8978, 72.3450)
+
+
+names = ("Alpha", "Bravo")
+
+print(names.count("Alpha"))
+print(names.index("Bravo"))
+
+# Sets
+
+days = {"M", "T", "W", "M"}
+print(days)
+
+days.add("F")
+print(days)
+
+days.discard("M")
+print(days)
+
+set1 = {1,3,5,7,10,13}
+set2 = {2,4,6.9,10}
+
+set3 = set1.union(set2)
+print(set3)
+
+print(set1.intersection(set2))
