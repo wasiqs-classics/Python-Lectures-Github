d78b7318fae0f865291f1bb5742bef5be3ce5906
wasiqs-classics <was_iq87@hotmail.com>
2024-11-30 18:10:02 +0500

    search engine implemented

---
+++ Assignments and Solutions/search-engine.py
@@ -0,0 +1,38 @@
+# Variable for search query
+# a list that represents our database
+# we will try to find matching keywords and then display the results containing those keywords
+
+print()
+print("Welcome to our Books Search Engine")
+print("============================")
+
+db = ["Python for Beginners", "Advanced JAVA", "C++ For Windows", "Pyhton book for Data Science", "C / C++ Fundamentals"]
+words_to_omit = ["or", "and", "the", "a", "an", "for", "in", "on"]
+
+query = input("Enter your search keywords >> ")
+
+query_list = query.split(' ')
+
+query_result = []
+
+for keyword in query_list:
+    if keyword.lower() in words_to_omit:
+        continue
+    else:
+        for db_item in db:
+            db_item_words = db_item.split(' ')
+            for words_in_current_db_item in db_item_words:
+                if keyword.lower() in words_in_current_db_item.lower():
+                    query_result.append(db_item)
+
+if len(query_result) != 0:
+    print()
+    print(f"Displaying {len(query_result)} items: ")
+    for result in query_result:
+        print(result)
+else:
+    print("No result found")
+
+
+
+
